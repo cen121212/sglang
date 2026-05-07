8ce75c474c71260a9e95653f10e28e03fc9f975a
cen121212 <ershoucen@163.com>
2026-05-07 11:19:38 +0800

    test

---
+++ boss/start.sh
@@ -0,0 +1,30 @@
+docker run -itd --shm-size=2048g --privileged=true --name sglang_lc_b110_new \
+--privileged=true --net=host \
+-v /usr/bin/hccn_tool:/usr/bin/hccn_tool \
+-v /usr/local/Ascend/driver/tools/hccn_tool:/usr/local/Ascend/driver/tools/hccn_tool \
+-v /usr/local/sbin:/usr/local/sbin \
+-v /home:/home \
+-v /mnt:/mnt \
+-v /usr/local/bin/npu-smi:/usr/local/bin/npu-smi \
+-v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+-v /usr/local/Ascend/firmware:/usr/local/Ascend/firmware \
+--device=/dev/davinci0:/dev/davinci0 \
+--device=/dev/davinci1:/dev/davinci1 \
+--device=/dev/davinci2:/dev/davinci2 \
+--device=/dev/davinci3:/dev/davinci3 \
+--device=/dev/davinci4:/dev/davinci4 \
+--device=/dev/davinci5:/dev/davinci5 \
+--device=/dev/davinci6:/dev/davinci6 \
+--device=/dev/davinci7:/dev/davinci7 \
+--device=/dev/davinci8:/dev/davinci8 \
+--device=/dev/davinci9:/dev/davinci9 \
+--device=/dev/davinci10:/dev/davinci10 \
+--device=/dev/davinci11:/dev/davinci11 \
+--device=/dev/davinci12:/dev/davinci12 \
+--device=/dev/davinci13:/dev/davinci13 \
+--device=/dev/davinci14:/dev/davinci14 \
+--device=/dev/davinci15:/dev/davinci15 \
+--device=/dev/davinci_manager:/dev/davinci_manager \
+--device=/dev/devmm_svm:/dev/devmm_svm \
+--device=/dev/hisi_hdc:/dev/hisi_hdc \
+swr.cn-southwest-2.myhuaweicloud.com/base_image/dockerhub/lmsysorg/sglang:cann8.5.0-a3-B110
